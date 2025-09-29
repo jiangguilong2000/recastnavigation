89c648a134f6850a72a4c777206ecde4163efd51
jiangguilong2000 <41157121@qq.com>
2025-09-29 03:56:00 +0000

    Deploying to gh-pages from @ jiangguilong2000/recastnavigation@64cd07a9fe257d9facb217a14e0ad86659a19990 🚀

--- classNavMesh.js
+++ classNavMesh.js
@@ -5,7 +5,7 @@ var classNavMesh =
     [ "GetInstace", "classNavMesh.html#adb29758d8bf6d61ecac547b7108f0eb7", null ],
     [ "New", "classNavMesh.html#add9c21dda6c39fa6b7769a16ecbbb9bc", null ],
     [ "Remove", "classNavMesh.html#ac9e9a9caef75695306e0a615ad4ff4e1", null ],
-    [ "Version", "classNavMesh.html#a0429ce96d1377061cd4fa56e769a82c5", null ],
+    [ "Version", "classNavMesh.html#a3f580b7d0f6935b75349ca592887407e", null ],
     [ "instance", "classNavMesh.html#af43f96091432fbf45f19ddba460e6498", null ],
     [ "navMeshContexs", "classNavMesh.html#ab60e15c6894f8a5fc174f850cefc1c35", null ]
 ];
